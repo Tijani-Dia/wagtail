93c7303ce620b1144b9b7d00448e0a0d3dacbe1c
Tidiane Dia <atdia97@gmail.com>
2022-10-22 22:54:47 +0100

    Add lazy_property

--- wagtail/coreutils.py
+++ wagtail/coreutils.py
@@ -539,3 +539,16 @@ class BatchCreator(BatchProcessor):
     def get_summary(self):
         opts = self.model._meta
         return f"{self.created_count}/{self.added_count} {opts.verbose_name_plural} were created successfully."
+
+
+class lazy_property:
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            value = self.func(instance)
+            setattr(instance, self.func.__name__, value)
+            return value
